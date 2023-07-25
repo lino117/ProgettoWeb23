75f3e4df6acae5c7c934dc71d57d2f5b2b9285db
jinclaudio <jinclaudio@gmail.com>
2023-07-25 18:11:18 +0200

    aggiunto JWT per memorizzare il login

--- router_Handler/users.js
+++ router_Handler/users.js
@@ -2,228 +2,236 @@ const connect = require('../database/mongodb')
 const jwt = require('jsonwebtoken');
 
 // true if noUser
-        noUser = async (username)=>{
-                try {
-                        const db = await connect();
-                        const collection = await db.collection('users');
-                        const result = await collection.find({username:username}).toArray();
-                        return (result.length === 0)
-                }   catch (e) {
-                        console.log(e)
-                }
-        }
+noUser = async (username) => {
+    try {
+        const db = await connect();
+        const collection = await db.collection('users');
+        const result = await collection.find({username: username}).toArray();
+        return (result.length === 0)
+    } catch (e) {
+        console.log(e)
+    }
+}
 
-        timeCounter =async ()=>{
-                const timeElapsed = Date.now();
-                return new Date(timeElapsed).toDateString()
-        }
+timeCounter = async () => {
+    const timeElapsed = Date.now();
+    return new Date(timeElapsed).toDateString()
+}
 
-        popularityCounter = async (username)=> {
-                const today = timeCounter()
-                try {
-                        var likeCont, dislikeCont;
-                        const db = await connect();
-                        const squeals = await db.collection('squeals')
-                        // parte che distingue popolarita del messaggio
-                        const messages = await squeals.find({usename: username})
-                         // await messages.each()
-                         //  // await squeals.find({usename: username}).each(async (err, doc)=>{
-                         //          // if(messages.like > CM && squealinfo.dislike > CM) {
-                         //          //         popularity = 'controverso'
-                         //          // }else if (squealinfo.like > CM){
-                         //          //         popularity = 'popolare'
-                         //          // }else if (squealinfo.dislike > CM){
-                         //          //         popularity = 'impopolare'
-                         //          // }
-                         //          if (doc){
-                         //                  console.log(doc)
-                         //          }else{
-                         //                  return false
-                         //          }
-
-
+popularityCounter = async (username) => {
+    const today = timeCounter()
+    try {
+        var likeCont, dislikeCont;
+        const db = await connect();
+        const squeals = await db.collection('squeals')
+        // parte che distingue popolarita del messaggio
+        const messages = await squeals.find({usename: username})
+        // await messages.each()
+        //  // await squeals.find({usename: username}).each(async (err, doc)=>{
+        //          // if(messages.like > CM && squealinfo.dislike > CM) {
+        //          //         popularity = 'controverso'
+        //          // }else if (squealinfo.like > CM){
+        //          //         popularity = 'popolare'
+        //          // }else if (squealinfo.dislike > CM){
+        //          //         popularity = 'impopolare'
+        //          // }
+        //          if (doc){
+        //                  console.log(doc)
+        //          }else{
+        //                  return false
+        //          }
 
 
+        // return await squeals.aggregate([
+        //         {
+        //                 $match:
+        //                     { username: username,time:{$gt:today} }
+        //         },
+        //         {
+        //                 $group: {_id: '$popularity', 'statusPopularity': {$count: {}}}
+        //         },
+        //         {
+        //                 $sort: {_id: 1}
+        //         }
+        // ]).toArray()
+        //  array sorted: contro, impop, pop, undefined
 
-                          // return await squeals.aggregate([
-                          //         {
-                          //                 $match:
-                          //                     { username: username,time:{$gt:today} }
-                          //         },
-                          //         {
-                          //                 $group: {_id: '$popularity', 'statusPopularity': {$count: {}}}
-                          //         },
-                          //         {
-                          //                 $sort: {_id: 1}
-                          //         }
-                          // ]).toArray()
-                          //  array sorted: contro, impop, pop, undefined
+    } catch (e) {
+        console.log(e)
+    }
 
-                  }catch (e) {
-                          console.log(e)
-                  }
-
-        }
-exports.test=async (req,res)=>{
-        const userinfo = req.body
-        res.send(popularityCounter(req.username))
+}
+exports.test = async (req, res) => {
+    const userinfo = req.body
+    res.send(popularityCounter(req.username))
 
 }
 
 // 参数是 username，password，usertype，Charnum,quote,if usertype= professional then smm else null,
-exports.regUser = async (req,res)=>{
-        const userinfo = req.body
+exports.regUser = async (req, res) => {
+    const userinfo = req.body
 
-        if(!userinfo.username || !userinfo.password){
-                return res.send( {status : 1, message:'Username o password non valido'} )
-        }
-        // const noUser = await noUser(userinfo.username)
+    if (!userinfo.username || !userinfo.password) {
+        return res.send({status: 1, message: 'Username o password non valido'})
+    }
+    // const noUser = await noUser(userinfo.username)
 
-        // if (noUser) {
-                try {
-                        const db = await connect();
-                        const collection = db.collection('users');
-                        collection.insertOne({
-                                username: userinfo.username,
-                                password: userinfo.password,
-                                usertype: userinfo.usertype,
-                                creditTot   : 1000,
-                                creditAvailable : 1000,
-                                SMM     : userinfo.smm,
-                                popSqueals: 0,
-                                umpopSqueals: 0,
-                        })
+    // if (noUser) {
+    try {
+        const db = await connect();
+        const collection = db.collection('users');
+        collection.insertOne({
+            username: userinfo.username,
+            password: userinfo.password,
+            usertype: userinfo.usertype,
+            creditTot: 1000,
+            creditAvailable: 1000,
+            SMM: userinfo.smm,
+            popSqueals: 0,
+            umpopSqueals: 0,
+        })
 
-                } catch (err) {
-                        console.log(err)
-                }
-                res.send('Registrato!')
-        // }else {
-        //         res.send('Username già esistente')
-        // }
+    } catch (err) {
+        console.log(err)
+    }
+    res.send('Registrato!')
+    // }else {
+    //         res.send('Username già esistente')
+    // }
 
 }
 //参数是 username password
-exports.login = async (req,res)=>{
-        const userinfo = req.body
-        try {
-                const db = await connect();
-                const collection = db.collection('users');
-                const result = await collection.find({
-                       username:userinfo.username,
-                       password:userinfo.password
-                }).toArray();
-                if (!result){
-                        return res.status(401).json({ error: "Utente non esiste o password sbagliata"})
-                }
-                const token = jwt.sign({ id: result._id, username: result.username }, 'tecweb2223', {
-                        expiresIn: '1h',
-                });
-                res.json({token})
-                // res.send((result.length === 0 ?  'Username o password errato, riprova oppure registra':result ))
-
-        } catch (err) {
-                console.error(err);
-                res.status(500).json({error: "Errore interno server"})
+exports.login = async (req, res) => {
+    const userinfo = req.body
+    try {
+        const db = await connect();
+        const collection = db.collection('users');
+        const result = await collection.find({
+            username: userinfo.username,
+            password: userinfo.password
+        }).toArray();
+        if (!result) {
+            return res.status(401).json({error: "Utente non esiste o password sbagliata"})
         }
+        const token = jwt.sign({id: result._id, username: userinfo.username}, 'tecweb2223', {
+            expiresIn: '1h',
+        });
+        res.json({token})
+        // res.send((result.length === 0 ?  'Username o password errato, riprova oppure registra':result ))
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: "Errore interno server"})
+    }
 
 }
 
 
 //参数是 username， newpsw
-exports.resetpsw= async (req,res)=>{
-        const userinfo = req.body
+exports.resetpsw = async (req, res) => {
+    const userinfo = req.body
 
-        if (userinfo.userphase === 1){
-                const noUser = await noUser(userinfo.username);
-               res.send(noUser ? 'Username inesistente' : userinfo.username)
-                // ricorda di mettere con bottone flag !userphase
-        }else{
-                try{
-                        const db = await connect();
-                        const collection = db.collection('users');
-                        // collection.findOneAndReplace({username:userinfo.username},{#})
+    if (userinfo.userphase === 1) {
+        const noUser = await noUser(userinfo.username);
+        res.send(noUser ? 'Username inesistente' : userinfo.username)
+        // ricorda di mettere con bottone flag !userphase
+    } else {
+        try {
+            const db = await connect();
+            const collection = db.collection('users');
+            // collection.findOneAndReplace({username:userinfo.username},{#})
 
-                        await collection.updateOne({username:userinfo.username},{$set:{password:userinfo.newpsw}})
-                        const result = await collection.find({username:userinfo.username}).toArray()
-                        res.send(result)
+            await collection.updateOne({username: userinfo.username}, {$set: {password: userinfo.newpsw}})
+            const result = await collection.find({username: userinfo.username}).toArray()
+            res.send(result)
 
-                }catch (err){
-                        console.log(err)
-                }
+        } catch (err) {
+            console.log(err)
         }
+    }
 
 }
 // 参数是 username, text, like, dislike,category,visitors
-exports.squeal=async (req,res)=> {
-        const squealinfo = req.body
-        const today = timeCounter();
+exports.squeal = async (req, res) => {
+    const squealinfo = req.body
+    const today = timeCounter();
 
 
-        const CM = squealinfo.visitors * 0.25
-        let popularity = undefined
+    const CM = squealinfo.visitors * 0.25
+    let popularity = undefined
 
 
-
-        if ( await noUser(squealinfo.username) === false ) {
-                try {
-                        const db = await connect();
-                        const squeals = await db.collection('squeals');
-                        const squeal = await squeals.insertOne({
-                                user    : squealinfo.username,
-                                text    : squealinfo.text,
-                                like    : squealinfo.like,
-                                dislike : squealinfo.dislike,
-                                category: squealinfo.category,
-                                time    : today,
-                                popularity : popularity,
-                                CM : CM
-                        })
-
-
-
-                        // parte che modifica quota caratteri dopo squeal
-                        const users = await db.collection('users')
-                        const User = await users.find({username: squealinfo.username}).toArray()
-                        var RemainderChar = User[0].CharNum - squealinfo.text.length
-                        await users.updateOne({username: squealinfo.username}, {$set: {CharNum: RemainderChar}})
-                        // aggiorna situazione popolarita
-                        const situationPop = popularityCounter(squealinfo.username)
-                        // if (User[0].popSqueals != situationPop[])
-
-
-
-                        res.send('squealed!')
-
-                } catch (e) {
-                        console.log(e)
-                }
-        }else{
-                res.send('username inesistente')
-        }
-
-}
-exports.getUser=async (req,res)=>{
-        const userinfo = req.query
-
-        try{
-                const db = await connect()
-                const users = await db.collection('users')
-                const result = await users.find({username:userinfo.username}).toArray()
-                res.send(result)
-
-        }catch (e) {
-                console.log(e)
-        }
-}
-exports.getSqueal = async (req,res)=>{
+    if (await noUser(squealinfo.username) === false) {
         try {
-                const db = await connect();
-                const squeals = await db.collection('squeals');
-                const messages = await squeals.find({}).toArray();
-                res.send(messages)
+            const db = await connect();
+            const squeals = await db.collection('squeals');
+            const squeal = await squeals.insertOne({
+                user: squealinfo.username,
+                text: squealinfo.text,
+                like: squealinfo.like,
+                dislike: squealinfo.dislike,
+                category: squealinfo.category,
+                time: today,
+                popularity: popularity,
+                CM: CM
+            })
 
-        }catch (e) {
-                console.log(e)
+
+            // parte che modifica quota caratteri dopo squeal
+            const users = await db.collection('users')
+            const User = await users.find({username: squealinfo.username}).toArray()
+            var RemainderChar = User[0].CharNum - squealinfo.text.length
+            await users.updateOne({username: squealinfo.username}, {$set: {CharNum: RemainderChar}})
+            // aggiorna situazione popolarita
+            const situationPop = popularityCounter(squealinfo.username)
+            // if (User[0].popSqueals != situationPop[])
+
+
+            res.send('squealed!')
+
+        } catch (e) {
+            console.log(e)
         }
+    } else {
+        res.send('username inesistente')
+    }
+
+}
+exports.getUser = async (req, res) => {
+    try {
+        const token = req.header('Authorization');
+        if (!token) {
+            return res.status(401).json({error: 'No token provided'});
+        }
+
+        const decoded = jwt.verify(token.replace('Bearer ', ''), 'tecweb2223');
+
+        const currentUser = decoded.username;
+        console.log('Current user:', currentUser);
+
+        const db = await connect();
+        const users = await db.collection('users');
+        const result = await users.find({ username: currentUser }).toArray();
+        console.log('Query result:', result);
+
+        res.send(result);
+    } catch (err) {
+        if (err.name === 'JsonWebTokenError' || err.name === 'TokenExpiredError') {
+            return res.status(403).json({ error: 'Invalid token' });
+        }
+        console.log(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+exports.getSqueal = async (req, res) => {
+    try {
+        const db = await connect();
+        const squeals = await db.collection('squeals');
+        const messages = await squeals.find({}).toArray();
+        res.send(messages)
+
+    } catch (e) {
+        console.log(e)
+    }
 }
